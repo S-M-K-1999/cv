f86722385de95853bad2973332f303733c7b3e14
sarang mk <sarangmonuttan@gmail.com>
2021-06-04 17:55:44 +0530

    with education

--- cv/urls.py
+++ cv/urls.py
@@ -2,5 +2,5 @@ from django import urls
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('portfolio/',views.index,name="portfolio")
+    path('',views.index,name="portfolio")
 ]
